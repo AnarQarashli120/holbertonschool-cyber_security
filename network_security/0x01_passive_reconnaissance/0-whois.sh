aa3da215500e4fc0a369bf8e47972ef656e76636
AnarQarashli120 <anarqarasli@gmail.com>
2025-12-27 20:31:01 +0000

    extract using whois scan in csv format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-whois $1|awk -F': ' '/^Registrant |^Admin |^Tech /{gsub(/^Registry /,"");print $1","$2}'>$1.csv
+
+whois "$1" | awk -F':' '
+/^(Registrant|Admin|Tech)/ {
+
+    section=$1
+    field=$2
+    value=$0
+
+    sub(/^[^:]*:[[:space:]]*/, "", value)
+
+    # Street fields → sonda SPACE olmalıdır
+    if (field ~ /Street/) {
+        printf "%s %s,%s \n", section, field, value
+        next
+    }
+
+    # Phone Ext və Fax Ext → colon qalmalıdır
+    if (field ~ /Phone Ext|Fax Ext/) {
+        printf "%s %s:, %s\n", section, field, value
+        next
+    }
+
+    printf "%s %s,%s\n", section, field, value
+}
+END {
+    # extra newline olmaması üçün heç nə etmirik
+}
+' > "$1.csv"
