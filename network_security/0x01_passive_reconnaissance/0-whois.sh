567ce40f0876a8455b92658331aeb4945312ef67
AnarQarashli120 <anarqarasli@gmail.com>
2025-12-27 20:43:49 +0000

    extract using whois scan in csv format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': *' '/^(Registrant|Admin|Tech)/{s=$1;f=$2;v=$3;if(f=="Street")printf "%s %s, %s\n",s,f,v;else if(f=="Phone Ext"||f=="Fax Ext")printf "%s %s:, %s\n",s,f,v;else printf "%s %s, %s\n",s,f,v}' > "$1.csv"
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
